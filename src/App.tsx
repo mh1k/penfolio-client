c71e572b0ee7450d2aba319ab9db7cf8e3dc25a6
mh1k <mahmudulhasan1033@gmail.com>
2025-01-31 20:32:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,156 @@
+import Navbar from "./components/shared/Navbar";
+
+const App = () => {
+  return (
+    <div className=" font-inter">
+      <Navbar></Navbar>
+      <div className="mx-10 ">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+          corporis illo assumenda sed et eum earum numquam veniam quod? Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          temporibus quis saepe minima dolor nostrum velit ea enim quos debitis.
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default App;
